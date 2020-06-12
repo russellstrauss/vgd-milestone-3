aa83d5648c4daa278695344f9f079a87362a7cf5
Russell Strauss <russellstrauss@gmail.com>
2020-06-12 15:37:51 -0400

    Add simple game flow menus

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+	
+	public void StartGame() {
+		Debug.Log("Button Press");
+		SceneManager.LoadScene("demo");
+	}
+	
+	public void ExitGame() {
+		Application.Quit();
+	}
+	
+	public void RestartLevel() {
+		SceneManager.LoadScene("demo");
+	}
+}
---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuController : MonoBehaviour
+{
+	private CanvasGroup canvasGroup;
+	
+	void Start() {
+		
+	} 
+	
+	void Awake() {
+		canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	void Update() {
+		
+		if (Input.GetKeyUp(KeyCode.Escape)) {
+			
+			if (canvasGroup.interactable) {
+				canvasGroup.interactable = false;
+				canvasGroup.blocksRaycasts = false;
+				canvasGroup.alpha = 0f;
+			}
+			else {
+				canvasGroup.interactable = true;
+				canvasGroup.blocksRaycasts = true;
+				canvasGroup.alpha = 1f;
+			}
+		}
+	}
+}
